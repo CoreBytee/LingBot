cd264c13e1b6657ddb1b55c7e0cf60488ac52233
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-03-25 15:57:05 +0100

    remove old

--- Config.lua
+++
@@ -1,9 +0,0 @@
-return {
-    Guild = "806963221469724723",
-    Channel = "931475048051658762",
-    Admin = "806964002000863282",
-
-    Token = require("./Token"),
-
-    Words = require("./Words/Loader.lua")()
-}
--- deps/discordia-interactions/libs/client/EventHandler.lua
+++
@@ -1,9 +0,0 @@
-local Interaction = require("containers/Interaction")
-local events = {}
-
-events.INTERACTION_CREATE = function(d, client)
-  local interaction = Interaction(d, client)
-  return client:emit("interactionCreate", interaction)
-end
-
-return events
