6a592947627a4cf78156f3b8918cfe1737155d50
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-03-29 09:14:56 +0200

    Update Main.js

--- src/Main/js/me/corebyte/LingBot/Main.js
+++ src/Main/js/me/corebyte/LingBot/Main.js
@@ -1,17 +1,42 @@
 const FS = require("fs-extra")
 const DiscordJs = require("discord.js")
 
-globalThis.LingBot = {
-    Words: Import("me.corebyte.LingBot.Words"),
-    Config: FS.readJSONSync("config.json"),
-    Client: new DiscordJs.Client(
-        {
-            intents: [3276799],
-        }
-    ),
-    Commands: {
-        Ping: Import("me.corebyte.LingBot.Commands.PingCommand")
+module.exports = async function() {
+    
+    globalThis.LingBot = {
+        Words: Import("me.corebyte.LingBot.Words"),
+        Config: FS.readJSONSync("config.json"),
+        Client: new DiscordJs.Client(
+            {
+                intents: [3276799],
+            }
+        ),
+        REST: new DiscordJs.REST({ version: "10" }),
+        Commands: [
+            Import("me.corebyte.LingBot.Commands.PingCommand")
+        ],
+        Emojis: {
+            Blocks: {
+                Blue: ":blue_square:",
+                Brown: ":brown_square:",
+                Green: ":green_square:",
+                Orange: ":orange_square:",
+                Purple: ":purple_square:",
+                Red: ":red_square:",
+                White: ":white_large_square:",
+                Yellow: ":yellow_square:",
+            }
+        },
+        WordsManager: Import("me.corebyte.LingBot.WordsManager")
     }
-}
+    LingBot.REST.setToken(LingBot.Config.Token)
 
-LingBot.Client.login(LingBot.Config.Token);
+    await LingBot.Client.login(LingBot.Config.Token);
+    await Import("me.corebyte.LingBot.DeployCommands")()
+    LingBot.Channel = await LingBot.Client.channels.fetch(LingBot.Config.ChannelId)
+    LingBot.Guild = LingBot.Channel.guild
+    await LingBot.WordsManager.LoadHandles()   
+    await LingBot.WordsManager.NewWord()
+
+
+}
