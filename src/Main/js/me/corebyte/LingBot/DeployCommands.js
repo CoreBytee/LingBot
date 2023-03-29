6faac1bacae2106494fc26aa193d38228e5598e2
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-03-29 09:15:19 +0200

    deploy command module

---
+++ src/Main/js/me/corebyte/LingBot/DeployCommands.js
@@ -0,0 +1,20 @@
+const DiscordJs = require("discord.js")
+
+module.exports = async function() {
+    TypeWriter.Logger.Information("Deploying commands...")
+    LingBot.REST.put(
+        DiscordJs.Routes.applicationCommands(LingBot.Client.user.id),
+        { body: LingBot.Commands.map(Command => Command.Data.toJSON()) }
+    )
+    TypeWriter.Logger.Information("Deployed commands.")
+
+    LingBot.Client.on(
+        "interactionCreate",
+        async function(Interaction) {
+            if (!Interaction.isCommand()) return
+            const Command = LingBot.Commands.filter(Command => Command.Data.name === Interaction.commandName)[0]
+            if (!Command) return
+            await Command.Execute(Interaction)
+        }
+    )
+}
