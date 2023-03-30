173401d5cd14cbed54ebc441106b615eaf223182
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-03-30 08:52:56 +0200

    deploy commands only to server

--- src/Main/js/me/corebyte/LingBot/DeployCommands.js
+++ src/Main/js/me/corebyte/LingBot/DeployCommands.js
@@ -3,7 +3,7 @@ const DiscordJs = require("discord.js")
 module.exports = async function() {
     TypeWriter.Logger.Information("Deploying commands...")
     LingBot.REST.put(
-        DiscordJs.Routes.applicationCommands(LingBot.Client.user.id),
+        DiscordJs.Routes.applicationGuildCommands(LingBot.Client.user.id, LingBot.Config.ServerId),
         { body: LingBot.Commands.map(Command => Command.Data.toJSON()) }
     )
     TypeWriter.Logger.Information("Deployed commands.")
