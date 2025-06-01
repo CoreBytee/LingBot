bf5b389b2ba44640ea2288db5c58b559154cf109
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-06-01 23:00:52 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import words from "../words.json";
+import { env } from "bun";
+import { Client, IntentsBitField } from "discord.js";
+import fetchEmoji from "./helpers/fetchEmoji";
+
+const emoji = await fetchEmoji();
+
+const discord = new Client({
+	intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages],
+});
+await discord.login(env.DISCORD_BOT_TOKEN as string);
+console.log(words);
